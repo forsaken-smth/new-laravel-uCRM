5ecf134baa18ee7f674643871184becfdc44d145
forsaken-smth <onigiri19194545@gmail.com>
2024-07-15 23:54:38 +0900

    first commit

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div v-if="$page.props.flash.status === 'success'"
+    class="bg-blue-300 text-white p-4 font-bold tracking-wider">
+        {{ $page.props.flash.message }}
+    </div>
+    <div v-if="$page.props.flash.status === 'denger'"
+    class="bg-red-500 text-white p-4 font-bold tracking-wider">
+        {{ $page.props.flash.message }}
+    </div>
+</template>
---
+++ resources/js/Components/ValidationErrors.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed, onMounted } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+
+const props = defineProps({
+    errors: Object
+})
+
+</script>
+
+<template>
+    <div v-if="Object.keys(props.errors).length > 0">
+        <div class="font-medium text-red-600">問題が発生しました。</div>
+
+        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+            <li v-for="error in errors" :key="error.key">{{ error }}</li>
+        </ul>
+    </div>
+</template>
